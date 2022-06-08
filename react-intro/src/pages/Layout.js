72355486fc48537671a912807d533fdf1a63ff82
Hassaan Zahid <hassaan.zahid@invozone.com>
2022-06-08 20:08:55 +0500

    redux-crud-complete

---
+++ react-intro/src/pages/Layout.js
@@ -0,0 +1,31 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+          <li>
+            <Link to="/react-state">React State</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
---
+++ reactHooks-main/src/component/useContext/ComB.js
@@ -0,0 +1,7 @@
+import ComC from "./ComC";
+
+const ComB = () => {
+    return <ComC />
+}
+
+export default ComB
